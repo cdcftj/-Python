dd31fdd4ee372c6eae6d8c066aa1f199c6bd1c40
Tan Jin <tanjin2004@126.com>
2018-11-06 16:11:16 +0800

    change

---
+++ xxcx.py
@@ -0,0 +1,9 @@
+print("=========我爱鱼C工作室")
+temp=input("心里想的数字：")
+guess=int(temp)
+if guess==8:
+    print("我是蛔虫")
+    print("中了也没有奖励")
+else:
+    print("错了，应该是8")
+print("游戏结束")
---
+++ 变量.py
@@ -0,0 +1,15 @@
+teacher='小谭'
+print(teacher)
+
+first=3
+second=8
+third=first+second
+print(third)
+
+#字符串拼接
+myteacher='小谭'
+youteacher='黑夜'
+ourteacher=myteacher+youteacher
+print(ourteacher)
+
+#使用变量前要赋值
