c5e0c4d30d7360a36598845079a476e97bfcf2da
Tan Jin <tanjin2004@126.com>
2018-11-09 23:18:58 +0800

    change

--- p5条件判断.py
+++ p5条件判断.py
@@ -1,9 +1,16 @@
 print("=========我爱鱼C工作室")
 temp=input("心里想的数字：")
 guess=int(temp)
-if guess==8:
-    print("我是蛔虫")
-    print("中了也没有奖励")
-else:
-    print("错了，应该是8")
+while guess != 8:
+    temp=input("出错了：")
+    guess=int(temp)
+    #条件分支语法，while循环
+    if guess==8:
+        print("你是蛔虫")
+        print("中了也没有奖励")
+    else:
+        if guess > 8:
+            print("哥，大了大了")
+        else:
+            print("黑，小了！小了")
 print("游戏结束")
