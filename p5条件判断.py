882b6b71e9033ad7711bb287a12806f5275b96bd
Tan Jin <tanjin2004@126.com>
2018-11-12 22:35:23 +0800

    change

--- p5条件判断.py
+++ p5条件判断.py
@@ -1,15 +1,17 @@
+import random #random模块，整形函数randint
+secret = random.randint(1,10)
 print("=========我爱鱼C工作室")
 temp=input("心里想的数字：")
 guess=int(temp)
-while guess != 8:
+while guess != secret:
     temp=input("出错了：")
     guess=int(temp)
     #条件分支语法，while循环
-    if guess==8:
+    if guess==secret:
         print("你是蛔虫")
         print("中了也没有奖励")
     else:
-        if guess > 8:
+        if guess > secret:
             print("哥，大了大了")
         else:
             print("黑，小了！小了")
