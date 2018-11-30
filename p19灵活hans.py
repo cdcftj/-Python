8cd38e4251a8a0a842704d6a1d59cff7d79cb1f2
Tan Jin <tanjin2004@126.com>
2018-11-30 16:28:26 +0800

    离散数学

---
+++ p19灵活hans.py
@@ -0,0 +1,13 @@
+#  离散数学题，在1-1000000中有多少个数字包括0或者1
+a = 0
+c = 0
+while a < 1000000:
+    a += 1
+    b = str(a)
+#    list(b)
+#    print(list(b))
+#    print('0' in b or '1' in b)
+    if ('0' in b or '1' in b):
+        c += 1
+        print(c)
+
