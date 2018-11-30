c973bda32e99cabfed0403575ef1e110e89f3f52
Tan Jin <tanjin2004@126.com>
2018-11-30 16:47:42 +0800

    离散数学

--- p19灵活函数.py
+++ p19灵活函数.py
@@ -1,7 +1,8 @@
 #  离散数学题，在1-1000000中有多少个数字包括0或者1
 a = 0
-c = 0
-while a < 1000000:
+c: int = 0
+for i in range(1,1000000):
+#while a < 1000000:
     a += 1
     b = str(a)
 #    list(b)
@@ -9,5 +10,4 @@ while a < 1000000:
 #    print('0' in b or '1' in b)
     if ('0' in b or '1' in b):
         c += 1
-        print(c)
-
+print(c)
