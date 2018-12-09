7a063d2457330bff7d41a01fb4215fe8570d85fd
Tan Jin <tanjin2004@126.com>
2018-12-09 23:12:09 +0800

    change

--- p22匿名函数lambda.py
+++ p22匿名函数lambda.py
@@ -58,3 +58,34 @@ def factorial(n):
 number = int(input('请输入一个正整数'))
 result = factorial(number)
 print("%d 的阶乘是：%d" %(number,result))
+
+# 兔子问题：斐波那契数列 递归 和 迭代实现
+def fab(n):
+    n1 = 1
+    n2 = 1
+    n3 = 1
+
+    if n < 1:
+        print('输入有误！')
+        return -1
+    while (n-2) > 0:
+        n3 = n2 + n1
+        n1 = n2
+        n2 = n3
+        n -= 1
+    return n3
+result = fab(20)
+if result != -1:
+    print('总共有%d对小兔崽子诞生!' % result)
+# 递归实现，两个递归
+def fab(n):
+    if n < 1:
+        return -1
+    if n ==1 or n ==2:
+        return 1
+    else:
+        return fab(n-1) + fab(n-2)
+result = fab(20)
+if result != -1:
+    print('总共有%d对小兔崽子诞生' % result)
+# 分制思想
