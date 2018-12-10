83002bc1cfe5792188569e6d1e6d7841fe807c21
Tan Jin <tanjin2004@126.com>
2018-12-10 22:44:33 +0800

    11.23更改

--- p25递归汗诺塔.py
+++ p25递归汗诺塔.py
@@ -8,3 +8,30 @@ def hanoi(n, x, y, z):
         hanoi(n-1, y, x, z)# 将y上的n-1个盘子移动到z上
 n = int(input('请输入汗诺塔的层数：'))
 hanoi(n, 'x', 'y', 'z')
+
+# p26 字典：当索引不好用时
+# 创建和访问字典
+brand = ['李宁', '耐克', '鱼c工作室']
+slogan =['一切皆有可能', 'Just do it ', '让编程改变世界']
+print('鱼c工作室的口号是：', slogan[brand.index('鱼c工作室')])
+# 建字典,字典是映射类型,李宁是key 冒号后面value
+dict1 = {'李宁': '一切皆有可能', '耐克': 'Just do it', '鱼c工作室': '让编程改变世界'}
+print('鱼c工作室的口号是：', dict1['鱼c工作室'])
+
+dict2 = {1: 'one', 2: 'two', 3: 'three'}
+print(dict2[2])
+# 空字典
+dict3 = {}
+dict3 = dict((('F', 70), ('i', 105), ('s', 115)))
+print(dict3)
+# 关键字也可以
+dict4 = dict(小甲鱼='让编程改变世界',李宁='一切皆有可能')
+print(dict4)
+# 改变值 ,替换了
+dict4['李宁'] = '一切都完蛋了'
+print(dict4)
+# 增加项
+dict4['成都天气'] = '现在2018年12月10日四度非常冷'
+print(dict4)
+
+
