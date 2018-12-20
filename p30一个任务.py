a627550afbc22ff625de3b7b02549ad5205990f3
cdcftj <tanjin2004@126.com>
2018-12-21 00:43:44 +0800

    change

---
+++ p30一个任务.py
@@ -0,0 +1,42 @@
+# 任务：将文件（record.tx）中的数据进行分割并按照以下规律保存起来：
+def save_file(boy, girl, count):
+    file_name_boy = 'boy_' + str(count) + '.txt'
+    file_name_girl = 'girl' + str(count) + '.txt'
+
+    boy_file = open(file_name_boy, 'w')
+    girl_file = open(file_name_girl)
+
+    boy_file.writelines(boy)
+    girl_file.writelines(girl)
+
+    boy_file.close()
+    girl_file.close()
+
+def split_file(file_name):
+    f = open('record.txt')
+
+    boy = []
+    girl = []
+    count = 1
+
+    for each_line in f:
+        if each_line[:6] != '======':
+            (role, line_spoken) = each_line.split(':',1)
+            if role == '小甲鱼' :
+                boy.append(line_spoken)
+            if role == '小客服':
+                girl.append(line_spoken)
+        else:
+            save_file(boy, girl, count)
+
+            boy = []
+            girl = []
+            count += 1
+    save_file(boy, girl, count)
+
+    f.close()
+
+#下面保存工作
+
+split_file('record.txt')
+
