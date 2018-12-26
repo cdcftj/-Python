06a8e4444b725e46aff66ba02e9766c5d315e178
cdcftj <tanjin2004@126.com>
2018-12-26 23:46:35 +0800

    change

---
+++ p31文件系统：高大上的东西.py
@@ -0,0 +1,16 @@
+# 模块是包含所有定义函数和变量的文件后缀是.py 引入random
+import  random
+secret = random.randint(1, 10)
+print(secret)
+
+# OS模块
+import  os
+print(os.getcwd())
+
+os.mkdir('c:\\a')
+# remove rename system
+# os.path 模块
+os.path.basename('E:\A')
+
+
+
